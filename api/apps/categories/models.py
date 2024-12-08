e54fa6e870b62c7986b9b26f1e56e0116971411f
NascimentoFrancisco <fmilk9049@gmail.com>
2024-12-08 12:37:00 -0300

    feat: Implementing Category model and its respective tests

--- api/apps/categories/models.py
+++ api/apps/categories/models.py
@@ -1,3 +1,22 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    """Category model"""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+    class Meta:
+        """Meta Class"""
+
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
