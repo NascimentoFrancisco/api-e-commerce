51d8899352fedc7ec0fd24d59a750cf09d10cdd1
NascimentoFrancisco <fmilk9049@gmail.com>
2024-12-16 15:29:05 -0300

    feat: Implementing ShoppingCart moel and testing it

--- api/apps/shopping_cart/models.py
+++ api/apps/shopping_cart/models.py
@@ -1,3 +1,27 @@
+import uuid
 from django.db import models
+from django.conf import settings
+from api.apps.products.models import Product
 
-# Create your models here.
+
+class ShoppingCart(models.Model):
+    """ShoppingCart Model"""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, null=False, blank=False
+    )
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> None:
+        return f"{self.product.name}"
+
+    class Meta:
+        """Meta class"""
+
+        ordering = ["created_at"]
+        verbose_name = "Carrinho"
+        verbose_name_plural = "carrinhos"
