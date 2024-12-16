4bcf8022d0780ee4446ee5de825179c71f5f65a5
NascimentoFrancisco <fmilk9049@gmail.com>
2024-12-16 16:57:03 -0300

    feat: Implementing View and serializer for the ShoppingCart model, and the respective tests

--- api/apps/shopping_cart/views.py
+++ api/apps/shopping_cart/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# pylint: disable=no-member
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from api.apps.shopping_cart.models import ShoppingCart
+from api.apps.shopping_cart.serializers import ShoppingCartSerializer
 
-# Create your views here.
+
+class ShoppingCartView(viewsets.ModelViewSet):
+    """View class for the ShoppingCart model"""
+
+    queryset = ShoppingCart.objects.all()
+    serializer_class = ShoppingCartSerializer
+    permission_classes = [IsAuthenticated]
