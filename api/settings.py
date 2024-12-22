55069bcd042240c50e52eac4f1536c97eaec2a61
NascimentoFrancisco <fmilk9049@gmail.com>
2024-12-22 14:00:00 -0300

    feat: Implementing CORS configuration in the API

--- api/settings.py
+++ api/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "drf_yasg",
+    "corsheaders",
     # Local Apps
     "api.apps.categories",
     "api.apps.products",
@@ -55,12 +56,15 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [config("CORS_ALLOWED_ORIGINS")]
 
 AUTH_USER_MODEL = "user.User"
 ROOT_URLCONF = "api.urls"
